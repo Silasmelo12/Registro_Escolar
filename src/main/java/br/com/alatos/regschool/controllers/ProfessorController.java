07b810f8133f6cd9a7de23a4ee38d2bd6f6513cd
Silas Melo <silasmelo12@gmail.com>
2022-07-27 01:34:18 -0300

    Página de detalhes do professor adicionada e quando o é adicionado um novo professor, a página de detalhes do mesmo é mostrada.

--- src/main/java/br/com/alatos/regschool/controllers/ProfessorController.java
+++ src/main/java/br/com/alatos/regschool/controllers/ProfessorController.java
@@ -7,14 +7,12 @@ import br.com.alatos.regschool.repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("professores")
@@ -51,7 +49,21 @@ public class ProfessorController {
         } else {
             Professor professor = requisicaoNovoProfessor.toProfessor();
             this.professorRepository.save(professor);
-            return new ModelAndView("redirect:professores");
+            return new ModelAndView("redirect:professores/"+professor.getId());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView show(@PathVariable Long id){
+        Optional<Professor> optional = this.professorRepository.findById(id);
+        if (optional.isPresent()){
+            Professor professor = optional.get();
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("professor",professor);
+            return mv;
+        }else {
+            System.out.println("Não achou nenhum optional com este Id");
+            return new ModelAndView("redirect:");
         }
     }
 }
