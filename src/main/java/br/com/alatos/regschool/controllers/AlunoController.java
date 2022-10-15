9827353665e37bcb3f5b6b6faa46ce3feeb165d4
Silasmelo12 <silasmelo12@gmail.com>
2022-10-14 21:20:46 -0300

    Cria AlunoController

--- src/main/java/br/com/alatos/regschool/controllers/AlunoController.java
+++ src/main/java/br/com/alatos/regschool/controllers/AlunoController.java
@@ -1,2 +1,12 @@
-package br.com.alatos.regschool.controllers;public class AlunoController {
+package br.com.alatos.regschool.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("alunos")
+public class AlunoController {
+
+
+
 }
