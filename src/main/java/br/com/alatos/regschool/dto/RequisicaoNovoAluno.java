43009c88c52c673f2c5f45192d582001acfcbd87
Silasmelo12 <silasmelo12@gmail.com>
2022-10-14 21:19:25 -0300

    Cria RequisicaoNovoAluno

--- src/main/java/br/com/alatos/regschool/dto/RequisicaoNovoAluno.java
+++ src/main/java/br/com/alatos/regschool/dto/RequisicaoNovoAluno.java
@@ -1,2 +1,17 @@
-package br.com.alatos.regschool.dto;public class RequisicaoNovoAluno {
+package br.com.alatos.regschool.dto;
+
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class RequisicaoNovoAluno {
+
+    @NotNull
+    @NotBlank
+    String nome;
+    @NotNull
+    @NotBlank
+    String ano;
+    @NotNull
+    int idade;
 }
